bf8f1b86c1d4368e33cd025c9e41e67aef718f0e
padge81 <stuart@padgettservices.com.au>
2026-01-17 12:22:10 +1100

    Update __init__.py

--- backend/core/edid/__init__.py
+++ backend/core/edid/__init__.py
@@ -2,10 +2,7 @@ from .read import read_edid_drm
 from .write import write_edid
 from .checksum import validate_edid, validate_checksum
 from .decode import decode_basic, edid_to_hex
-from .compare import    (
-                            find_matching_edid, 
-                            edid_hash,
-                        )
+from .compare import find_matching_edid, edid_hash
 from .diff import diff_edid
 from .save import save_edid
 from .exceptions import (
