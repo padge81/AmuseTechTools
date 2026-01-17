b1926c7319f5e14fd8331ea624acc6fe50a3961d
padge81 <stuart@padgettservices.com.au>
2026-01-17 16:48:42 +1100

    Update __init__.py

--- backend/core/edid/__init__.py
+++ backend/core/edid/__init__.py
@@ -1,29 +1,43 @@
 from .read import read_edid_drm
 from .write import write_edid
+from .save import save_edid
 from .checksum import validate_edid, validate_checksum
 from .decode import decode_basic, edid_to_hex
-from .compare import find_matching_edid, edid_hash
+from .compare import (
+    find_matching_edid,
+    edid_matches,
+    edid_hash,
+)
 from .diff import diff_edid
-from .save import save_edid
 from .exceptions import (
-                            EDIDError,
-                            EDIDReadError,
-                            EDIDWriteError,
-                        )
+    EDIDError,
+    EDIDReadError,
+    EDIDWriteError,
+)
 
 __all__ = [
+    # Read / Write
     "read_edid_drm",
     "write_edid",
+    "save_edid",
+
+    # Validation
     "validate_edid",
     "validate_checksum",
+
+    # Decode / display
     "decode_basic",
     "edid_to_hex",
+
+    # Compare / match
     "find_matching_edid",
-    "edid_hash",
     "edid_matches",
-    "list_saved_edids",
+    "edid_hash",
+
+    # Diff
     "diff_edid",
-    "save_edid",
+
+    # Errors
     "EDIDError",
     "EDIDReadError",
     "EDIDWriteError",
