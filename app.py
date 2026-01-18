2f6ce3adee18ed5b286a386f531c686ac469ec1b
padge81 <stuart@padgettservices.com.au>
2026-01-18 17:39:58 +1100

    Update app.py

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def system(action):
     return jsonify(system_action(action))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
