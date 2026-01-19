a7fb146b6b04cb33eb200c45adbe8b3b679712c0
padge81 <stuart@padgettservices.com.au>
2026-01-20 07:25:38 +1100

    Update system.js

--- frontend/static/js/system.js
+++ frontend/static/js/system.js
@@ -1,10 +1,40 @@
 function systemAction(action) {
+    let message = "";
+
+    switch (action) {
+        case "exit_browser":
+            message = "Are you sure you want to exit the browser?";
+            break;
+        case "reboot":
+            message = "Are you sure you want to reboot the Raspberry Pi?";
+            break;
+        case "shutdown":
+            message = "Are you sure you want to shut down the Raspberry Pi?";
+            break;
+        case "update":
+            message = "Update from GitHub now?";
+            break;
+        default:
+            return;
+    }
+
+    if (!confirm(message)) {
+        return;
+    }
+
     fetch(`/system/${action}`, {
         method: "POST"
     })
-    .then(r => {
-        if (!r.ok) throw new Error("Request failed");
-        return r.json();
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Request failed");
+        }
+        return response.text();
     })
-    .catch(err => alert(err));
+    .then(text => {
+        alert(text);
+    })
+    .catch(err => {
+        alert("Error: " + err.message);
+    });
 }
