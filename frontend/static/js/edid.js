1a4011868bb830d6f39cfae8e215905c54fd9e77
padge81 <stuart@padgettservices.com.au>
2026-01-21 06:51:07 +1100

    Update edid.js

--- frontend/static/js/edid.js
+++ frontend/static/js/edid.js
@@ -5,7 +5,7 @@ function setStatus(text) {
 function readEdid() {
     setStatus("Reading EDID...");
 
-    fetch("/edid/read?connector=card0-HDMI-A-1")
+fetch("/edid/read?connector=card0-HDMI-A-1")
     .then(res => res.json())
     .then(data => {
         console.log("EDID READ RESPONSE:", data);
@@ -13,27 +13,31 @@ function readEdid() {
     .catch(err => {
         console.error("EDID READ ERROR:", err);
     });
-            if (!data.ok) {
-                throw new Error(data.error);
-            }
 
-            document.getElementById("output").innerText =
-                data.edid_hex.match(/.{1,32}/g).join("\n");
-
-            if (data.matches.length > 0) {
-                document.getElementById("match").innerText =
-                    "✔ Match: " + data.matches[0].filename;
-            } else {
-                document.getElementById("match").innerText =
-                    "❌ No match found";
-            }
-
-            setStatus("Read OK");
-        })
-        .catch(err => {
-            setStatus("Error");
-            alert(err.message);
-        });
+ //   fetch("/edid/read?connector=card0-HDMI-A-1")
+ //       .then(r => r.json())
+ //       .then(data => {
+ //           if (!data.ok) {
+ //               throw new Error(data.error);
+ //           }
+//
+ //           document.getElementById("output").innerText =
+//                data.edid_hex.match(/.{1,32}/g).join("\n");
+//
+//            if (data.matches.length > 0) {
+//                document.getElementById("match").innerText =
+ //                   "✔ Match: " + data.matches[0].filename;
+ //           } else {
+ //               document.getElementById("match").innerText =
+ //                   "❌ No match found";
+ //           }
+//
+//            setStatus("Read OK");
+ //       })
+//        .catch(err => {
+//           setStatus("Error");
+ //           alert(err.message);
+ //       });
 }
 
 function resetView() {
