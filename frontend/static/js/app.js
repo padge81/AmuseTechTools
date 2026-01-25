e9b6f992695a4341d22d3c5cbed68b02988fac7b
padge81 <stuart@padgettservices.com.au>
2026-01-26 09:57:56 +1100

    Update app.js

---
+++ frontend/static/js/app.js
@@ -0,0 +1,9 @@
+function navHome() {
+    window.location.href = "/";
+}
+
+function navBack() {
+    // For now: same behavior
+    window.history.back();
+    // Or: window.location.href = "/";
+}
