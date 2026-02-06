e998087f0b2fbd5bd527adc621d88ba5dc75796d
padge81 <stuart@padgettservices.com.au>
2026-02-06 20:48:59 +1100

    Create pykms_probe.py

---
+++ scripts/pykms_probe.py
@@ -0,0 +1,15 @@
+python3 - << 'EOF'
+import pykms
+
+card = pykms.Card()
+print("Card opened")
+
+res = pykms.ResourceManager(card)
+print("ResourceManager:", res)
+
+print("\nAttributes of ResourceManager:")
+print(dir(res))
+
+print("\nCard attributes:")
+print(dir(card))
+EOF
